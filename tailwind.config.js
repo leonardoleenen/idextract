b32f5483d34d172b87f2c5b3e1b488c740f72dc7
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2020-04-26 19:16:50 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          '500':'var(--color-primary-500)',
+        },
+        secondary: {
+          '300':'var(--color-secondary-300)',
+          '400':'var(--color-secondary-400)',
+          '500':'var(--color-secondary-500)',
+        },
+        terciary: {
+          '500':'var(--color-terciary-500)',
+        },
+        bgScreen: {
+          '500':'var(--color-bgScreen-500)',
+        },
+        btnText: {
+          '500':'var(--color-btnText-500)',
+        }
+      },
+      borderRadius: {
+        full:'var(--border-500)',
+      },
+    }
+  },
+}
